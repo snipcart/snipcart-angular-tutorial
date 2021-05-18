58ad944ac74382e6d921282fcc0620ac5522ccc2
Ludovic Armand <77077373+ludovic-armand@users.noreply.github.com>
2021-05-18 13:54:56 -0400

    Update app.component.ts
    Capitalized title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Infinite summer ice cream store';
+  title = 'Infinite Summer Ice Cream Store';
   subtitle = 'Which one do you want?';
 }
