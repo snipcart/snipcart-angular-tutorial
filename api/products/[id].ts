14750e5b59dc5e15248d019eb8627a02962e7547
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2021-05-20 08:58:13 -0400

    Update serverless dependencies imports

--- api/products/[id].ts
+++ api/products/[id].ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { Product } from 'src/app/core/product';
-import { PRODUCTS } from 'src/app/mock-products';
+import { Product } from '../../src/app/core/product';
+import { PRODUCTS } from '../../src/app/mock-products';
 
 const findProduct = (id: number): Product | undefined =>
   PRODUCTS.find((product) => product.id === id);
@@ -22,11 +22,8 @@ export default function fetchProductInfo(
   req: VercelRequest,
   res: VercelResponse
 ) {
-  console.log('alll');
   const id = Number(req.query.id);
   const product = findProduct(id);
-  const flavorOptions = getFlavorOptions(product);
-  const sizeOptions = getSizeOptions(product);
   res.statusCode = 200;
   res.send({
     id: id,
